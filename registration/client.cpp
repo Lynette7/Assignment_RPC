0ea18983468603b15662c170e7641a41d59a778c
Lynette7 <lynettewanjiru7@gmail.com>
2023-06-14 09:00:04 +0300

    Added comments

--- registration/client.cpp
+++ registration/client.cpp
@@ -11,24 +11,31 @@ using registration::RegistrationRequest;
 using registration::RegistrationResponse;
 using registration::RegistrationService;
 
+// Client class for making RPC calls to the Registration service
 class RegistrationClient {
 public:
     RegistrationClient(std::shared_ptr<Channel> channel)
         : stub_(RegistrationService::NewStub(channel)) {}
 
+    // Method to process the registration request
     std::string ProcessRegistration(const std::string& registration, const std::string& name) {
+        // Create the request message
         RegistrationRequest request;
         request.set_registration(registration);
         request.set_name(name);
 
+        // Create the response object
         RegistrationResponse response;
         ClientContext context;
 
+        // Make the RPC call to the server
         Status status = stub_->ProcessRegistration(&context, request, &response);
 
         if (status.ok()) {
+            // Return the response message if the RPC call is successful
             return response.message();
         } else {
+            // Return an error message if the RPC call fails
             return "RPC failed.";
         }
     }
@@ -39,6 +46,8 @@ private:
 
 int main(int argc, char** argv) {
     std::string server_address("localhost:8888");
+
+    // Create a RegistrationClient instance with the gRPC channel
     RegistrationClient client(grpc::CreateChannel(server_address, grpc::InsecureChannelCredentials()));
 
     std::string registration, name;
@@ -47,6 +56,7 @@ int main(int argc, char** argv) {
     std::cout << "Enter name: ";
     std::cin >> name;
 
+    // Make an RPC call to the server
     std::string response = client.ProcessRegistration(registration, name);
     std::cout << "Response from server: " << response << std::endl;
 
